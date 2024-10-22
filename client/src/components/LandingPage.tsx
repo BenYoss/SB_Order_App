977a6dac2f34459542111f6aa25d62baa0f2fdc8
benyoss <benyoss4@gmail.com>
2024-10-21 20:01:03 -0500

    (style) landing to be more user friendly.

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,17 +1,25 @@
 import react from 'react';
 
-import Toolbar from './Toolbar';
+import Toolbar from './toolbar/Toolbar';
 
 export default function LandingPage() {
     return (
         <div id="landing-container">
-            <Toolbar />
+            <Toolbar session={null} />
             
             <div id="landing-header">
-                <h1 className="hdr large" id="landing-header-text">
-                    Have Products Delivered 
-                    <span className="hdr large emphasis">Globally</span> 
-                    In Passionate, Dedicated, and Talented Hands.
+                <h1 className="hdr giant" id="landing-header-text">
+                    Have Products Delivered
+                    <span className="hdr giant emphasis" id="landing-header-emphasis">{' Globally '}</span>
+                    <span className="landing-header-text-diag">
+                        In Passionate, 
+                    </span>
+                    <span className="landing-header-text-diag x2">
+                        Dedicated,
+                    </span> 
+                    <span className="landing-header-text-diag x3">
+                        and Talented Hands.
+                    </span>
                 </h1>
             </div>
             <div id="landing-body"></div>
