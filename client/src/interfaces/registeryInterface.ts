bcecdbadccf6eee85e2dd61e8061d792d30f92d7
benyoss <benyoss4@gmail.com>
2024-10-21 19:53:15 -0500

    (add) interfaces SoC for each component.

---
+++ client/src/interfaces/registeryInterface.ts
@@ -0,0 +1,17 @@
+interface UserInterface {
+    "username": string,
+    "email": string,
+    "phone": string,
+    "password": string,
+    "confirm": string,
+    "street": string,
+    "city": string,
+    "zip": string,
+    "country": string,
+    "credit": string,
+    "card": string,
+    "creditType": string
+};
+
+
+export { UserInterface };
