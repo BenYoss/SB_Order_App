afca7b8e265ac6ea4de079f6e3295cfa95b86667
benyoss <benyoss4@gmail.com>
2024-10-28 23:58:10 -0500

    (fix) ORM model to match DB instance.

--- server/internal/models/store_cart.go
+++ server/internal/models/store_cart.go
@@ -2,8 +2,11 @@ package models
 
 type StoreCart struct {
 	CartOwner       string `json:"cart_owner"`
+	ProductID       string `json:"product_id"`
 	ProductName     string `json:"product_name"`
 	ProductCategory string `json:"product_category"`
+	ProductImageURL string `json:"product_image_url"`
 	ProductColor    string `json:"product_color"`
+	Price           string `json:"price"`
 	DateAdded       string `json:"date_added"`
 }
